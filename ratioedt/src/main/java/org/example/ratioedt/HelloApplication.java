5e13d3c8ad2b667d254e6b0fe27c53749673f561
Mariateb <sabmou05@gmail.com>
2024-03-04 13:26:52 +0100

    Ajout CSS primitif

--- ratioedt/src/main/java/org/example/ratioedt/HelloApplication.java
+++ ratioedt/src/main/java/org/example/ratioedt/HelloApplication.java
@@ -3,9 +3,11 @@ package org.example.ratioedt;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
+import javafx.scene.text.Font;
 import javafx.stage.Stage;
 
 import java.io.IOException;
+import java.util.Objects;
 
 public class HelloApplication extends Application {
     @Override
@@ -14,6 +16,8 @@ public class HelloApplication extends Application {
         Scene scene = new Scene(fxmlLoader.load(), 500, 500);
         stage.setTitle("Hello!");
         stage.setScene(scene);
+        Font.loadFont(getClass().getResourceAsStream("ja-jp.ttf"), 36);
+        scene.getStylesheets().add(Objects.requireNonNull(getClass().getResource("style.css")).toExternalForm());
         stage.show();
     }
 
